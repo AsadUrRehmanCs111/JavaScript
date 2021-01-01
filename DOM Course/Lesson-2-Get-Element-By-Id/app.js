05e090106a878a4e4bc7076442a3dc93d9cfa87e
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-02 01:09:27 +0500

    ⚡️ Lesson 2 : Learn how to Get Element By ID

---
+++ DOM Course/Lesson-2-Get-Element-By-Id/app.js
@@ -0,0 +1,8 @@
+var wrapper = document.getElementById('wrapper');
+console.log(wrapper)
+var banner = document.getElementById('page-banner')
+console.log(banner)
+var form = document.getElementById('search-books')
+console.log(form)
+var listOfBooks = document.getElementById('book-list')
+console.log(listOfBooks)
