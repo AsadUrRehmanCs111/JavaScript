63767bc283bbe40f9bf1328daed19a7cad489e8f
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-04 00:16:32 +0500

    ⚡️ Lesson-13: About CLass and add styles to classes

---
+++ DOM Course/Lesson-13 Classes and Styles/app.js
@@ -0,0 +1,42 @@
+//Delete Books
+const lists = document.querySelector('#book-list ul')
+
+lists.addEventListener('click', function (e) {
+    if (e.target.className == 'delete') {
+        const li = e.target.parentNode
+        lists.removeChild(li)
+    }
+})
+
+
+//add book-lits 
+
+const addBooks = document.forms['add-book'];
+
+addBooks.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const value = addBooks.querySelector('input[type="text"]').value
+    console.log(value)
+
+    //Add ELements
+    const li = document.createElement('li')
+    const bookName = document.createElement('span')
+    const deleteBtn = document.createElement('span')
+
+    //add content
+
+    bookName.textContent = value
+    deleteBtn.textContent = 'delete'
+
+    //Styles and CLasses by classList 
+
+    bookName.classList.add('book')
+    deleteBtn.classList.add('delete')
+
+    //Append these Elements to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    lists.appendChild(li)
+
+})
+
